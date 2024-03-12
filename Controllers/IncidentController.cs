4db4a7a3e2dd0bd95a789724e095bd283e41c3d4
SohamNagi <nagisoham@gmail.com>
2024-03-12 11:59:00 -0400

    Init

---
+++ Controllers/IncidentController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace SNDCAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class IncidentController : ControllerBase
+{
+
+    private readonly string _serviceNowUrl;
+    private readonly string _username;
+    private readonly string _password;
+
+    public IncidentController(IConfiguration configuration)
+    {
+        _serviceNowUrl = configuration["ServiceNowUrl"]!;
+        _username = configuration["Username"]!;
+        _password = configuration["Password"]!;
+    }
+
+    
+    [HttpPost]
+    [Consumes("application/xml")]
+    public async Task<IActionResult> PostIncident([FromBody] Session session)
+    {
+        try
+        {
+
+            // Extract the relevant data and flatten to json
+            FormData flatData = new FormData();
+            flatData.u_brand = session.Data.Policy.BrandFlag;
+            flatData.u_policynumber = session.Data.Policy.PolicyNumber;
+            flatData.u_caseid = session.Data.Policy.WorkbenchCaseId;
+            flatData.u_businessunit = session.Data.Policy.OrganizationalUnitDropdown;
+            flatData.u_marketdimension = session.Data.Policy.MarketDimension;
+            flatData.u_transaction = session.Data.CurrentTransactionType;
+
+            // Convert Json to string
+            string jsonData = JsonSerializer.Serialize(flatData);
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(
+                "Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes($"{_username}:{_password}")));
+
+                var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync($"{_serviceNowUrl}/api/now/table/incident", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseBody = await response.Content.ReadAsStringAsync();
+                    var incident = JsonSerializer.Deserialize<Root>(responseBody);
+                    return Ok($"{_serviceNowUrl}{"/incident.do?number="}{incident.result.number}");
+
+                }
+                else
+                {
+                    return StatusCode((int)response.StatusCode,
+                                  "Error sending data: " + await response.Content.ReadAsStringAsync());
+                }
+            }      
+        }
+        catch (Exception ex)
+        {
+            return BadRequest("Error processing data: " + ex.Message);
+        }
+    }
+    
+}
+
---
+++ XmlData.cs
@@ -0,0 +1,173 @@
+﻿using System.Xml.Serialization;
+
+namespace SNDCAPI;
+
+[Serializable, XmlRoot("session")]
+public class Session
+{
+    [XmlElement("data")]
+    public Data? Data { get; set; }
+}
+
+public class Data
+{
+    [XmlElement("policy")]
+    public Policy? Policy { get; set; }
+
+    [XmlElement("CurrentTransactionType")]
+    public string? CurrentTransactionType { get; set; }
+}
+
+public class Policy
+{
+    [XmlElement("BrandFlag")]
+    public string? BrandFlag { get; set; }
+
+    [XmlElement("PolicyNumber")]
+    public string? PolicyNumber { get; set; }
+
+    [XmlElement("WorkbenchCaseId")]
+    public string? WorkbenchCaseId { get; set; }
+
+    [XmlElement("OrganizationalUnitDropdown")]
+    public string? OrganizationalUnitDropdown { get; set; }
+
+    [XmlElement("MarketDimension")]
+    public string? MarketDimension { get; set; }
+}
+
+public class FormData
+{
+    public const string u_application = "Wave";
+    public string? u_policynumber { get; set; }
+    public string? u_caseid { get; set; }
+    public string? u_brand { get; set; }
+    public string? u_businessunit { get; set; }
+    public string? u_marketdimension { get; set; }
+    public string? u_transaction { get; set; }
+    public FormData(string policyNumber, string caseID, string brand, string businessUnit, string marketDimension, string transaction)
+    {
+        u_policynumber = policyNumber;
+        u_caseid = caseID;
+        u_brand = brand;
+        u_businessunit = businessUnit;
+        u_marketdimension = marketDimension;
+        u_transaction = transaction;
+    }
+
+    public FormData() { }
+}
+
+public class OpenedBy
+{
+    public string Link { get; set; }
+    public string Value { get; set; }
+}
+
+public class Result
+{
+    public string parent { get; set; }
+    public string made_sla { get; set; }
+    public string caused_by { get; set; }
+    public string watch_list { get; set; }
+    public string upon_reject { get; set; }
+    public string sys_updated_on { get; set; }
+    public string child_incidents { get; set; }
+    public string hold_reason { get; set; }
+    public string origin_table { get; set; }
+    public string task_effective_number { get; set; }
+    public string approval_history { get; set; }
+    public string number { get; set; }
+    public string resolved_by { get; set; }
+    public string sys_updated_by { get; set; }
+    public OpenedBy opened_by { get; set; }
+    public string user_input { get; set; }
+    public string sys_created_on { get; set; }
+    public SysDomain sys_domain { get; set; }
+    public string state { get; set; }
+    public string route_reason { get; set; }
+    public string sys_created_by { get; set; }
+    public string knowledge { get; set; }
+    public string order { get; set; }
+    public string calendar_stc { get; set; }
+    public string closed_at { get; set; }
+    public string cmdb_ci { get; set; }
+    public string delivery_plan { get; set; }
+    public string contract { get; set; }
+    public string impact { get; set; }
+    public string active { get; set; }
+    public string work_notes_list { get; set; }
+    public string business_service { get; set; }
+    public string business_impact { get; set; }
+    public string priority { get; set; }
+    public string sys_domain_path { get; set; }
+    public string rfc { get; set; }
+    public string time_worked { get; set; }
+    public string expected_start { get; set; }
+    public string opened_at { get; set; }
+    public string business_duration { get; set; }
+    public string group_list { get; set; }
+    public string work_end { get; set; }
+    public string caller_id { get; set; }
+    public string reopened_time { get; set; }
+    public string resolved_at { get; set; }
+    public string approval_set { get; set; }
+    public string subcategory { get; set; }
+    public string work_notes { get; set; }
+    public string universal_request { get; set; }
+    public string short_description { get; set; }
+    public string close_code { get; set; }
+    public string correlation_display { get; set; }
+    public string delivery_task { get; set; }
+    public string work_start { get; set; }
+    public string assignment_group { get; set; }
+    public string additional_assignee_list { get; set; }
+    public string business_stc { get; set; }
+    public string cause { get; set; }
+    public string description { get; set; }
+    public string origin_id { get; set; }
+    public string calendar_duration { get; set; }
+    public string close_notes { get; set; }
+    public string notify { get; set; }
+    public string service_offering { get; set; }
+    public string sys_class_name { get; set; }
+    public string closed_by { get; set; }
+    public string follow_up { get; set; }
+    public string parent_incident { get; set; }
+    public string sys_id { get; set; }
+    public string contact_type { get; set; }
+    public string reopened_by { get; set; }
+    public string incident_state { get; set; }
+    public string urgency { get; set; }
+    public string problem_id { get; set; }
+    public string company { get; set; }
+    public string reassignment_count { get; set; }
+    public string activity_due { get; set; }
+    public string assigned_to { get; set; }
+    public string severity { get; set; }
+    public string comments { get; set; }
+    public string approval { get; set; }
+    public string sla_due { get; set; }
+    public string comments_and_work_notes { get; set; }
+    public string due_date { get; set; }
+    public string sys_mod_count { get; set; }
+    public string reopen_count { get; set; }
+    public string sys_tags { get; set; }
+    public string escalation { get; set; }
+    public string upon_approval { get; set; }
+    public string correlation_id { get; set; }
+    public string location { get; set; }
+    public string category { get; set; }
+}
+
+public class Root
+{
+    public Result result { get; set; }
+}
+
+public class SysDomain
+{
+    public string link { get; set; }
+    public string value { get; set; }
+}
+
